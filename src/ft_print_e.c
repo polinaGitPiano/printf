3e7ca97975e13e2020215c656a08af46d9839814
I love Polina <Klepickova.pol@yandex.ru>
2020-02-08 20:40:16 +0300

    printf

--- src/ft_print_e.c
+++ src/ft_print_e.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../includes/ft_printf.h"
 
 void	ft_set_exp(char *number, t_fnum *fnum, char spec)
 {
